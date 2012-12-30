337584c156267b139feb41ca06094738b6cb3f4e
Patrick Seidel <psycowithespn1@aim.com>
2012-12-30 01:41:23 -0500

    Add a string representation of each team.

--- src/me/chaseoes/tf2/Team.java
+++ src/me/chaseoes/tf2/Team.java
@@ -2,6 +2,16 @@ package me.chaseoes.tf2;
 
 public enum Team {
 
-    RED,
-    BLUE;
+    RED("red"),
+    BLUE("blue");
+
+    private final String name;
+
+    private Team (String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
