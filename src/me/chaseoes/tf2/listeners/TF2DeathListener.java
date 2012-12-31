9006490161e5532355eb282dd4336ceb8844ec1f
chaseoes <chase.oes@gmail.com>
2012-12-30 21:47:28 -0700

    Uhh... bugfixes?

--- src/me/chaseoes/tf2/listeners/TF2DeathListener.java
+++ src/me/chaseoes/tf2/listeners/TF2DeathListener.java
@@ -12,8 +12,6 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class TF2DeathListener implements Listener {
 
