3203fdacd0b0cfb7a38b521e0a4fcc83f5e67605
Patrick Seidel <psycowithespn1@aim.com>
2013-06-03 15:40:49 -0400

    Include killstreaks in suicide check, #34

--- src/main/java/me/chaseoes/tf2/listeners/TF2DeathListener.java
+++ src/main/java/me/chaseoes/tf2/listeners/TF2DeathListener.java
@@ -54,11 +54,11 @@ public class TF2DeathListener implements Listener {
                     killerg.setTotalKills(-1);
                     killer.setLevel(killerg.getTotalKills());
                     killerg.setKills(-1);
-                }
 
-                int kills = killerg.getKills();
-                if (kills % TF2.getInstance().getConfig().getInt("killstreaks") == 0) {
-                    game.broadcast(ChatColor.YELLOW + "[TF2] " + playerg.getTeamColor() + killer.getName() + " " + ChatColor.RESET + ChatColor.YELLOW + "is on a " + ChatColor.DARK_RED + ChatColor.BOLD + "" + kills + " " + ChatColor.RESET + ChatColor.YELLOW + "kill streak!");
+                    int kills = killerg.getKills();
+                    if (kills % TF2.getInstance().getConfig().getInt("killstreaks") == 0) {
+                        game.broadcast(ChatColor.YELLOW + "[TF2] " + playerg.getTeamColor() + killer.getName() + " " + ChatColor.RESET + ChatColor.YELLOW + "is on a " + ChatColor.DARK_RED + ChatColor.BOLD + "" + kills + " " + ChatColor.RESET + ChatColor.YELLOW + "kill streak!");
+                    }
                 }
 
                 player.setHealth(20);
