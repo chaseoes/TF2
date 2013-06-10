94721ff0fd787acd64a42977fdd4e445f1d9d8e7
chaseoes <chase.oes@gmail.com>
2013-06-09 19:08:45 -0700

    Ability to capture points in any order.

--- src/main/java/me/chaseoes/tf2/listeners/PlayerMoveListener.java
+++ src/main/java/me/chaseoes/tf2/listeners/PlayerMoveListener.java
@@ -19,53 +19,58 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMoveListener implements Listener {
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onMove(PlayerMoveEvent event) {
-        if (!(event.getFrom().getBlockX() != event.getTo().getBlockX() || event.getFrom().getBlockY() != event.getTo().getBlockY() || event.getFrom().getBlockZ() != event.getTo().getBlockZ())) {
-            return;
-        }
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onMove(PlayerMoveEvent event) {
+		if (!(event.getFrom().getBlockX() != event.getTo().getBlockX() || event.getFrom().getBlockY() != event.getTo().getBlockY() || event.getFrom().getBlockZ() != event.getTo().getBlockZ())) {
+			return;
+		}
 
-        if (GameUtilities.getUtilities().getGamePlayer(event.getPlayer()).isInLobby() || !GameUtilities.getUtilities().getGamePlayer(event.getPlayer()).isIngame()) {
-            return;
-        }
+		if (GameUtilities.getUtilities().getGamePlayer(event.getPlayer()).isInLobby() || !GameUtilities.getUtilities().getGamePlayer(event.getPlayer()).isIngame()) {
+			return;
+		}
 
-        Player player = event.getPlayer();
-        GamePlayer gp = GameUtilities.getUtilities().getGamePlayer(player);
-        Block b = event.getTo().getBlock();
-        TF2 plugin = TF2.getInstance();
+		Player player = event.getPlayer();
+		GamePlayer gp = GameUtilities.getUtilities().getGamePlayer(player);
+		Block b = event.getTo().getBlock();
+		TF2 plugin = TF2.getInstance();
 
-        Map map = plugin.getMap(CapturePointUtilities.getUtilities().getMapFromLocation(b.getLocation()));
-        if (map == null) {
-            return;
-        }
+		Map map = plugin.getMap(CapturePointUtilities.getUtilities().getMapFromLocation(b.getLocation()));
+		if (map == null) {
+			return;
+		}
 
-        // Capture Points
-        if (gp.isIngame() && GameUtilities.getUtilities().games.get(map.getName()).getStatus() == GameStatus.INGAME && CapturePointUtilities.getUtilities().locationIsCapturePoint(b.getLocation())) {
-            Integer id = CapturePointUtilities.getUtilities().getIDFromLocation(b.getLocation());
-            CapturePoint cp = map.getCapturePoint(id);
-            if (gp.getTeam() == Team.RED) {
-                if (cp.getStatus().string().equalsIgnoreCase("uncaptured")) {
-                    if (CapturePointUtilities.getUtilities().capturePointBeforeHasBeenCaptured(map, id)) {
-                        if (cp.capturing == null) {
-                            cp.startCapturing(gp);
-                        }
-                    } else {
-                        event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-MUST-CAPTURE-PREVIOUS").replace("%id", id - 1 + ""));
-                    }
-
-                } else if (map.getCapturePoint(id).getStatus().string().equalsIgnoreCase("captured")) {
-                    event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-ALREADY-CAPTURED-RED").replace("%id", id + 1 + ""));
-                } else if (map.getCapturePoint(id).getStatus().string().equalsIgnoreCase("capturing")) {
-                    event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-ALREADY-CAPTURING").replace("%player", cp.capturing.getName()));
-                }
-            } else {
-                if (!map.getCapturePoint(id).getStatus().string().equalsIgnoreCase("captured")) {
-                    event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-WRONG-TEAM"));
-                } else {
-                    event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-ALREADY-CAPTURED-BLUE"));
-                }
-            }
-        }
-    }
+		// Capture Points
+		if (gp.isIngame() && GameUtilities.getUtilities().games.get(map.getName()).getStatus() == GameStatus.INGAME && CapturePointUtilities.getUtilities().locationIsCapturePoint(b.getLocation())) {
+			Integer id = CapturePointUtilities.getUtilities().getIDFromLocation(b.getLocation());
+			CapturePoint cp = map.getCapturePoint(id);
+			if (gp.getTeam() == Team.RED) {
+				if (cp.getStatus().string().equalsIgnoreCase("uncaptured")) {
+					if (TF2.getInstance().getConfig().getBoolean("capture-in-order")) {
+						if (CapturePointUtilities.getUtilities().capturePointBeforeHasBeenCaptured(map, id)) {
+							if (cp.capturing == null) {
+								cp.startCapturing(gp);
+							}
+						} else {
+							event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-MUST-CAPTURE-PREVIOUS").replace("%id", id - 1 + ""));
+						}
+					} else {
+						if (cp.capturing == null) {
+							cp.startCapturing(gp);
+						}
+					}
+				} else if (map.getCapturePoint(id).getStatus().string().equalsIgnoreCase("captured")) {
+					event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-ALREADY-CAPTURED-RED").replace("%id", id + 1 + ""));
+				} else if (map.getCapturePoint(id).getStatus().string().equalsIgnoreCase("capturing")) {
+					event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-ALREADY-CAPTURING").replace("%player", cp.capturing.getName()));
+				}
+			} else {
+				if (!map.getCapturePoint(id).getStatus().string().equalsIgnoreCase("captured")) {
+					event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-WRONG-TEAM"));
+				} else {
+					event.getPlayer().sendMessage(Localizer.getLocalizer().loadPrefixedMessage("CP-ALREADY-CAPTURED-BLUE"));
+				}
+			}
+		}
+	}
 
 }
